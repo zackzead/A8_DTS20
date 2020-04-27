37d029a61b064b51ed6040d98d485bf4b3c8c8e1
Ahmad Miftahul Zaki <39727014+zackzead@users.noreply.github.com>
2020-04-27 22:20:30 +0700

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,23 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openLogin() {
+  document.getElementById('id01').style.display='block';
+}
+
+function closeLogin() {
+  document.getElementById('id01').style.display='none';
+}
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
